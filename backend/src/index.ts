729a47dad5507f85aa28cd37432eb622fc742593
Dolapo-18 <ogunyemi.dolapo@gmail.com>
2024-03-08 19:57:33 +0100

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => { console.log("Connected to DB")})
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/my/user', myUserRoute);
+
+app.listen(7001, () => {
+    console.log(`Server running on port 7001`)
+})
